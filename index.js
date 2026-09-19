5864dd80198241ae47f420d654a868d472fa738a
Aditya Ghaywat <adityaghaywat34@gmail.com>
2026-09-19 14:14:59 +0530

    Fix HTML response in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('<h1>hello aditya my first node1 project is deploy use pipeline</h1>')
+      <h2> 1.0.0</h2>
 })
 
 app.listen(port,'0.0.0.0', () => {
