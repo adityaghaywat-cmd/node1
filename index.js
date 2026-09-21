5f621210ee7cad278bfb2be6860f844a4b48c828
Aditya Ghaywat <adityaghaywat34@gmail.com>
2026-09-21 14:02:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express')
 
 const app = express()
 const port = 3000
-
+https://github.com/adityaghaywat-cmd/node1/edit/main/index.js
 app.get('/', (req, res) => {
     res.send(`
         <h1>Hello Aditya 👋</h1>
         <h2>My first Node1 project is deployed using Jenkins Pipeline1</h2>
-        <h2>Version: 1.0.0</h2>
+        <h2>Version: 2.0.0</h2>
     `)
 })
 
